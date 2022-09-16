fb4aef466be411192fc697f41dca128c7bca2155
unknown <nguyenngan09112000@gmail.com>
2022-09-16 09:44:58 +0700

    commit change

--- src/index.js
+++ src/index.js
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { Route, Router, Switch } from 'react-router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider>
+    <Router>
+      <React.Suspense fallback={<Loading />}>
+        <Switch>
+          <Route path="/" component={App}>
+            <Route
+              path="/"
+            />
+          </Route>
+        </Switch>
+      </React.Suspense>
+    </Router>
+  </Provider>
 );
 reportWebVitals();
